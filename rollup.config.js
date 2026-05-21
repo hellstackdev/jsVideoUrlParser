09f591a7bea0c6f8f2764410689c59c772ec4019
jeffrey-omega <>
2026-05-21 20:12:54 +0200

    Update rollup config to disable generating source maps

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,7 @@ export default [
       name: 'urlParser',
       file: 'dist/jsVideoUrlParser.js',
       format: 'umd',
-      sourcemap: true,
+
     },
     plugins: basePlugins,
   },
@@ -29,7 +29,6 @@ export default [
       name: 'urlParser',
       file: 'dist/jsVideoUrlParser.min.js',
       format: 'umd',
-      sourcemap: true,
     },
     plugins: [...basePlugins, terser()],
   },
@@ -38,7 +37,6 @@ export default [
     output: {
       file: 'dist/jsVideoUrlParser.esm.js',
       format: 'es',
-      sourcemap: true,
       exports: 'default',
     },
     plugins: basePlugins,
